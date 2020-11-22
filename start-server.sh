8df1fb262f4d00b20cfbc15440747159c85df588
root <vigneshsweekaran@gmail.com>
2020-11-22 18:21:32 +0000

    update

--- start-server.sh
+++ start-server.sh
@@ -3,5 +3,5 @@
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
     (cd profiles_api; python manage.py createsuperuser --no-input)
 fi
-(cd profiles_api; gunicorn profiles_api.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) &
+(cd profiles_api; gunicorn wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) &
 nginx -g "daemon off;"
